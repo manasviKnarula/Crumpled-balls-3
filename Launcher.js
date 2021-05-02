b989aaa85fbe9d0a63e6c871c724c1bb7de35027
manasviKnarula <79053669+manasviKnarula@users.noreply.github.com>
2021-05-01 22:18:08 -0400

    Add files via upload

---
+++ Launcher.js
@@ -0,0 +1,30 @@
+class launcher{
+    constructor(bodyA,pointB){
+        
+        var options = {
+            bodyA: bodyA,
+            pointB:pointB,
+            stiffness:0.01,
+            length :10
+            }
+        
+        this.pointB=pointB;
+        this.launch = Constraint.create(options);
+        World.add(world, this.launch);
+    }
+ fly()
+ {
+     this.launch.bodyA=null;
+ }
+
+    display(){
+       if(this.launch.bodyA){
+        var pointA = this.launch.bodyA.position;
+        var pointB = this.pointB;
+        strokeWeight(4);
+        stroke("black");
+        line(pointA.x, pointA.y, pointB.x,pointB.y);
+       }
+    
+} 
+}
---
+++ sketch.js
@@ -0,0 +1,67 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var dustbinObj, paperObject,groundObject;	
+var world;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	paperObject=new paper(200,450,70);
+	groundObject=new ground(width/2,670,width,20);
+	dustbinObj=new dustbin(1200,650);
+	launch = new launcher(paperObject.body,{x:400,y:350})
+
+	
+
+	
+	Engine.run(engine);
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+  
+  paperObject.display();
+  groundObject.display();
+  dustbinObj.display();
+  launch.display();
+
+  
+  drawSprites();
+ 
+  
+  
+ 
+}
+
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:130,y:-145});
+
+    
+  	}
+}
+
+function mouseDragged(){
+	Matter.Body.setPosition(paperObject.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+	launch.fly();
+}
+
